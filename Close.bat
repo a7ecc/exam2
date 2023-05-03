7ccf23cf07ba6b08254c169b9b2c92120267f6fb
a7ecc <102755240+a7ecc@users.noreply.github.com>
2023-05-03 15:58:32 +0400

    Delete Close.bat

--- Close.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-taskkill /f /im powershell.exe > nul
-taskkill /f /im ps.exe > nul
-taskkill /f /im telegram.exe > nul
